a651615b6571c077b8159bcd1d9226f7a1a7960c
Renu987 <renuka.yamala2422@gmail.com>
2025-02-20 19:12:48 +0530

    Update app.py

--- Project-DockerHub/app.py
+++ Project-DockerHub/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Welcome to Renuka's world!'
+    return "Hello, Welcome to Renuka's world!"
 
 if __name__ == '__main__':
     app.run()
